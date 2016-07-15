f3cfa2f4d63d33366213dd1ea5692852f9cd0d3b
Greg Funk <gfunk@summa-tech.com>
2016-07-15 16:43:28 -0400

    Removes seed code

--- app/app.js
+++ app/app.js
@@ -3,16 +3,10 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
   'ngRoute',
-  'ui.bootstrap',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ui.bootstrap'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
-}])
-.controller('ButtonsCtrl', function ($scope) {
-  $scope.singleModel = 1;
-});
+}]);

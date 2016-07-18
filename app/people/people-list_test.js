6079ec5dde67348699bf9eefdcb17c9a76025e96
Greg Funk <gfunk@summa-tech.com>
2016-07-18 09:41:41 -0400

    Gets unit testing working

--- app/people/people-list_test.js
+++ app/people/people-list_test.js
@@ -1,15 +1,17 @@
 'use strict';
 
-describe('People List', function() {
-
+describe('myApp.peopleList directive tests', function() {
   beforeEach(module('myApp'));
 
-  describe('people list directive', function(){
-
-    it('should define the directive', inject(function($controller) {
-      var peopleListDirective = $directive('peopleList');
-      expect(peopleListDirective).toBeDefined();
-    }));
-
+  describe('peopleList directive', function() {
+    it('should exist', function() {
+      module(function($provide) {
+        //$provide.value('version', 'TEST_VER');
+      });
+      inject(function($compile, $rootScope) {
+        var element = $compile('<people-list></people-list>')($rootScope);
+        expect(element).toBeDefined();
+      });
+    });
   });
 });

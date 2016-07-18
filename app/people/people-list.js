099d348eacc814b5291328dfbed423276643414e
Greg Funk <gfunk@summa-tech.com>
2016-07-18 15:21:25 -0400

    Add more people to the list and images for each person

--- app/people/people-list.js
+++ app/people/people-list.js
@@ -6,10 +6,28 @@ app.directive("peopleList", function(){
   return {
     restrict: 'E',
     templateUrl: 'people/people-list.html',
-    controller: function($scope) {
+    controller: function() {
       this.people = [
-        { name: "Iron Man"},
-        { name: "Thor"}
+        { name: 'Ant Man', image: '/images/ant-man.png'},
+        { name: 'Black Panther', image: '/images/black-panther.png'},
+        { name: 'Black Widow', image: '/images/black-widow.png'},
+        { name: 'Captain America', image: '/images/captain-america.jpg'},
+        { name: 'Daredevil', image: '/images/daredevil.jpg'},
+        { name: 'Drax The Destroyer', image: '/images/drax-the-destroyer.jpg'},
+        { name: 'Falcon', image: '/images/falcon.png'},
+        { name: 'Gamora', image: '/images/gamora.jpeg'},
+        { name: 'Groot', image: '/images/groot.jpg'},
+        { name: 'Hawkeye', image: '/images/hawkeye.jpg'},
+        { name: 'Hulk', image: '/images/hulk.jpg'},
+        { name: 'Iron Man', image: '/images/iron-man.jpg'},
+        { name: 'Quicksilver', image: '/images/quicksilver.jpg'},
+        { name: 'Rocket Raccoon', image: '/images/rocket-raccoon.jpg'},
+        { name: 'Scarlet Witch', image: '/images/scarlet-witch.jpg'},
+        { name: 'Spider-Man', image: '/images/spider-man.jpg'},
+        { name: 'Starlord', image: '/images/starlord.jpg'},
+        { name: 'Thor', image: '/images/thor.jpg'},
+        { name: 'Vision', image: '/images/vision.jpg'},
+        { name: 'Winter Soldier', image: '/images/winter-soldier.jpg'}
       ]
     },
     controllerAs: 'PeopleCtrl'

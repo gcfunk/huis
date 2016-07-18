5da7efe917db8c12b5debbda8daada3135c8067e
Greg Funk <gfunk@summa-tech.com>
2016-07-18 11:38:08 -0400

    Refactor - pull init code into a beforeEach block in people test

--- app/people/people-list_test.js
+++ app/people/people-list_test.js
@@ -1,24 +1,24 @@
 'use strict';
 
 describe('myApp.peopleList directive tests', function() {
-  var $scope;
+  var $scope, controller;
 
   beforeEach(module('myApp'));
 
-  describe('peopleList directive', function() {
-    it('should exist', function() {
-      inject(function($compile, $rootScope, $injector) {
-        var $httpBackend = $injector.get('$httpBackend');
-        $httpBackend.whenGET('people/people-list.html').respond(200, '');
-        var scope = $rootScope.$new();
-        var element = angular.element("<people-list></people-list>");
-        var template = $compile(element)(scope);
-        $httpBackend.flush();
-        scope.$digest();
-        var controller = element.controller('peopleList');
+  beforeEach(inject(function($compile, $rootScope, $injector) {
+    var $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.whenGET('people/people-list.html').respond(200, '');
+    var scope = $rootScope.$new();
+    var element = angular.element("<people-list></people-list>");
+    var template = $compile(element)(scope);
+    $httpBackend.flush();
+    scope.$digest();
+    controller = element.controller('peopleList');
+  }));
 
-        expect(controller.people).toBeDefined();
-      });
+  describe('peopleList directive', function() {
+    it('have a list of people', function() {
+      expect(controller.people).toBeDefined();
     });
   });
 });

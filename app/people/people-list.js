e63810d8529ef85b6c0d7fa34509cf61da284925
Greg Funk <gfunk@summa-tech.com>
2016-07-19 09:55:46 -0400

    Adds a gender filter to the people list

--- app/people/people-list.js
+++ app/people/people-list.js
@@ -8,30 +8,46 @@ app.directive("peopleList", function(){
     templateUrl: 'people/people-list.html',
     controller: function() {
       this.people = [
-        { name: 'Ant Man', image: '/images/ant-man.png'},
-        { name: 'Black Panther', image: '/images/black-panther.png'},
-        { name: 'Black Widow', image: '/images/black-widow.png'},
-        { name: 'Captain America', image: '/images/captain-america.jpg'},
-        { name: 'Daredevil', image: '/images/daredevil.jpg'},
-        { name: 'Drax The Destroyer', image: '/images/drax-the-destroyer.jpg'},
-        { name: 'Falcon', image: '/images/falcon.png'},
-        { name: 'Gamora', image: '/images/gamora.jpeg'},
-        { name: 'Groot', image: '/images/groot.jpg'},
-        { name: 'Hawkeye', image: '/images/hawkeye.jpg'},
-        { name: 'Hulk', image: '/images/hulk.jpg'},
-        { name: 'Iron Man', image: '/images/iron-man.jpg'},
-        { name: 'Quicksilver', image: '/images/quicksilver.jpg'},
-        { name: 'Rocket Raccoon', image: '/images/rocket-raccoon.jpg'},
-        { name: 'Scarlet Witch', image: '/images/scarlet-witch.jpg'},
-        { name: 'Spider-Man', image: '/images/spider-man.jpg'},
-        { name: 'Starlord', image: '/images/starlord.jpg'},
-        { name: 'Thor', image: '/images/thor.jpg'},
-        { name: 'Vision', image: '/images/vision.jpg'},
-        { name: 'Winter Soldier', image: '/images/winter-soldier.jpg'}
+        { name: 'Ant Man', image: '/images/ant-man.png', gender: 'male', details: ''},
+        { name: 'Black Panther', image: '/images/black-panther.png', gender: 'male', details: ''},
+        { name: 'Black Widow', image: '/images/black-widow.png', gender: 'female', details: ''},
+        { name: 'Captain America', image: '/images/captain-america.jpg', gender: 'male', details: ''},
+        { name: 'Daredevil', image: '/images/daredevil.jpg', gender: 'male', details: ''},
+        { name: 'Drax The Destroyer', image: '/images/drax-the-destroyer.jpg', gender: 'male', details: ''},
+        { name: 'Falcon', image: '/images/falcon.png', gender: 'male', details: ''},
+        { name: 'Gamora', image: '/images/gamora.jpeg', gender: 'female', details: ''},
+        { name: 'Groot', image: '/images/groot.jpg', gender: 'not specified', details: ''},
+        { name: 'Hawkeye', image: '/images/hawkeye.jpg', gender: 'male', details: ''},
+        { name: 'Hulk', image: '/images/hulk.jpg', gender: 'male', details: ''},
+        { name: 'Iron Man', image: '/images/iron-man.jpg', gender: 'male', details: ''},
+        { name: 'Quicksilver', image: '/images/quicksilver.jpg', gender: 'male', details: ''},
+        { name: 'Rocket Raccoon', image: '/images/rocket-raccoon.jpg', gender: 'male', details: ''},
+        { name: 'Scarlet Witch', image: '/images/scarlet-witch.jpg', gender: 'female', details: ''},
+        { name: 'Spider-Man', image: '/images/spider-man.jpg', gender: 'male', details: ''},
+        { name: 'Starlord', image: '/images/starlord.jpg', gender: 'male', details: ''},
+        { name: 'Thor', image: '/images/thor.jpg', gender: 'male', details: ''},
+        { name: 'Vision', image: '/images/vision.jpg', gender: 'not specified', details: ''},
+        { name: 'Winter Soldier', image: '/images/winter-soldier.jpg', gender: 'male', details: ''}
       ];
 
       this.peopleSort = 'name';
+
+      this.genderFilter = 'all';
     },
     controllerAs: 'PeopleCtrl'
   };
+})
+
+.filter('bygender', function() {
+  return function(people,genderFilter) {
+    var out = [];
+    for ( var i in people ) {
+      var person = people[i];
+      if( genderFilter === 'all'|| genderFilter === person.gender.toLowerCase() ) {
+        out.push(person);
+      }
+    }
+
+    return out;
+  }
 });

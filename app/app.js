cf91a54fd9b626bdafe18547cd180cc4e2d81e02
Greg Funk <gfunk@summa-tech.com>
2016-07-20 10:18:21 -0400

    Replaces ngRoute with ui.router

--- app/app.js
+++ app/app.js
@@ -2,10 +2,7 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'ui.bootstrap',
   'ngResource'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-}]);
+]);

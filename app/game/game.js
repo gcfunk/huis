b794518909b8adeaf59346ba9ee26f9a8c841759
Greg Funk <gfunk@summa-tech.com>
2016-07-20 17:30:56 -0400

    Adds game html and some styles, placeholder data

--- app/game/game.js
+++ app/game/game.js
@@ -8,9 +8,18 @@ app
   return {
     restrict: 'E',
     templateUrl: 'game/game.html',
-    controller: function() {
+    controller: ['People', function(People) {
+      var $this = this;
+      People.query(function(data){
+        $this.people = JSON.parse(angular.toJson(data));
+        $this.randomPerson();
+      });
 
-    },
+      this.randomPerson = function() {
+        $this.person = $this.people[0];
+        $this.choices = [{name: '1'},{name: '2'},{name: '3'},{name: '4'}];
+      }
+    }],
     controllerAs: 'GameCtrl'
   };
 })
--- app/game/game_test.js
+++ app/game/game_test.js
@@ -9,6 +9,13 @@ describe('myApp.peopleList directive tests', function() {
     // there's got to be an easier way to get an instance of the controller
     var $httpBackend = $injector.get('$httpBackend');
     $httpBackend.whenGET('game/game.html').respond(200, '');
+    $httpBackend.whenGET('/people/people.json').respond(200,
+      "[{" +
+      "\"name\": \"Ant Man\"," +
+      "\"image\": \"/images/ant-man.png\"," +
+      "\"gender\": \"male\"," +
+      "\"details\": \"details for ant man\"" +
+      "}]");
     var scope = $rootScope.$new();
     var element = angular.element("<game></game>");
     var template = $compile(element)(scope);
@@ -18,8 +25,17 @@ describe('myApp.peopleList directive tests', function() {
   }));
 
   describe('game directive', function() {
-    it('exists', function() {
-      expect(controller).toBeDefined();
+    it('has a list of people', function() {
+      expect(controller.people).toBeDefined();
+      expect(controller.people.length).toBeGreaterThan(0);
     });
+
+    it('has a single person for display', function() {
+      expect(controller.person).toBeDefined();
+    });
+
+    it('has a list of 4 choices', function(){
+      expect(controller.choices.length).toBe(4);
+    })
   });
 });

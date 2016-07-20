7c04e322fc2c50ba4331d1f877b7fcefd5f0b241
Greg Funk <gfunk@summa-tech.com>
2016-07-20 11:07:57 -0400

    Adds people list as a view for ui-router

--- app/app.js
+++ app/app.js
@@ -5,4 +5,12 @@ var app = angular.module('myApp', [
   'ui.router',
   'ui.bootstrap',
   'ngResource'
-]);
+]).config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise("/people-list");
+
+  $stateProvider
+    .state('people-list', {
+      url: '/people-list',
+      template: "<people-list></people-list>"
+    });
+});

7a2484efd3eafcb188daee666d427b1554dddab6
Greg Funk <gfunk@summa-tech.com>
2016-07-21 09:41:40 -0400

    Picks a random 4 people as choices, and then a random one of them for the correct choice

--- app/game/game.js
+++ app/game/game.js
@@ -16,9 +16,27 @@ app
       });
 
       this.randomPerson = function() {
-        var randomIndex = Math.floor((Math.random() * $this.people.length));
-        $this.person = $this.people[randomIndex];
-        $this.choices = [{name: '1'},{name: '2'},{name: '3'},{name: '4'}];
+        $this.choices = angular.copy($this.people);
+        var temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        for (var currentIndex = $this.choices.length - 1; currentIndex > 0; currentIndex--) {
+
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+
+          // And swap it with the current element.
+          temporaryValue = $this.choices[currentIndex];
+          $this.choices[currentIndex] = $this.choices[randomIndex];
+          $this.choices[randomIndex] = temporaryValue;
+        }
+
+        // take the first 4 people in the shuffled array as the choices
+        $this.choices = $this.choices.slice(0,4);
+
+        var randomPersonIndex = Math.floor((Math.random() * $this.choices.length));
+        $this.person = $this.choices[randomPersonIndex];
       }
     }],
     controllerAs: 'GameCtrl'
